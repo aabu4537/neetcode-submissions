2bc9bc28e5000fcdc27fd35d40cce5142dbab9de
Alam Bushara <89425995+aabu4537@users.noreply.github.com>
2026-08-05 10:04:01 -0500

    Add: course-schedule-ii - submission-7

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-7.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        graph = {i: [] for i in range(numCourses)}
+        indegree = {i: 0 for i in range(numCourses)}
+        res = []
+
+        for crs, pre in prerequisites:
+            graph[pre].append(crs)
+            indegree[crs] +=1
+        q = deque()
+
+
+        for crs in range(numCourses):
+            if indegree[crs] == 0:
+                q.append(crs)
+
+        while q:
+            node = q.popleft()
+            res.append(node)
+            for crs in graph[node]:
+                indegree[crs] -= 1
+                if indegree[crs] == 0:
+                    q.append(crs)
+
+        if len(res) == numCourses:
+            return res
+        else:
+            return []
+
+        
