c1ef083084c6cf1dda4feb191ff13d12da53a68c
Alam Bushara <89425995+aabu4537@users.noreply.github.com>
2026-08-25 10:03:25 -0500

    Add: count-number-of-islands - submission-22

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-22.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
+        directions = [(-1, 0), (1,0), (0,-1), (0,1)]
+        def helper(r,c):
+            if r < 0 or c< 0 or r >= ROWS or c >= COLS or grid[r][c] != "1":
+                return
+            
+            grid[r][c] = "0"
+
+            helper(r-1, c)
+            helper(r+1, c)
+            helper(r, c-1)
+            helper(r, c+1)
+                
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    helper(r,c)
+                    islands+=1
+        
+        return islands
+        
+        
